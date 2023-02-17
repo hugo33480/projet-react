1378963dfdc23b9ac3623a3122bb192e62d51799
hugo <hugobast33@gmail.com>
2023-02-17 10:45:42 +0100

    Router

---
+++ src/Components/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { Link } from 'react-router-dom';
+
+function Home() {
+  return (
+    <Link to="/Detail">Detail</Link>
+  );
+}
+
+export default Home;
